217a6efcdf59a002d9aeb6cde0377cf859225043
Tamilarasan <tamilarasan.p.91@gmail.com>
2021-04-12 18:33:57 +0530

    call back url change

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json({extended:true}));
 app.use(function(req,res,next){
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Access-Control-Allow-Origin');
     res.setHeader("Access-Control-Allow-Credentials",true);
     next();
 });
